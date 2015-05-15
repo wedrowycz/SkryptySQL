6ca3673e6424d51f6a54f19958f7d9722d535858
wedrowycz <tomaszjureczko@wp.pl>
2015-05-15 09:50:54 +0200

    inne

---
+++ numeracja kontrahentow.sql
@@ -0,0 +1,6 @@
+use pe
+select top 15 * from pekontrah order by sutworzony desc
+select top 10 * from pekontrah order by sdata  desc, sczas desc
+select * from atid where atablica = 'PEKONTRAH'
+
+--update atid set sdata = sdata where atablica = 'PEKONTRAH'
